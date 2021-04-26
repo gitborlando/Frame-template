4b750697a85f4526dc598b65341d130ce9548982
皮皮歪 <72959369+gitborlando@users.noreply.github.com>
2021-04-26 22:14:22 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    devServer: {
+        port: 8787
+    },
     plugins: [
         new htmlWebpackPlugin({
             template: './public/index.html',
